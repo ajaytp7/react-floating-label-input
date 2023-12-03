18484c621b4afb6f89a362aed8d622204c7f1dfa
AJAY T P <ajaytp7@gmail.com>
2023-12-03 20:40:12 +0530

    Update react-floating-label-input.tsx

--- src/react-floating-label-input.tsx
+++ src/react-floating-label-input.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import './style.module.css'
+// import './style.module.css'
 
 // export const ReactFloatingInput = styled.input`
 // background-color: green;
